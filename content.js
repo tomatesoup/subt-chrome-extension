42cbb61f9e88b50065694fa958444c9324cc031d
shsulliv <shsulliv@gmail.com>
2016-05-19 21:12:56 +0200

    Refactored code on content.js, removed jquery and updated for new youtube video player

--- content.js
+++ content.js
@@ -1,52 +1,40 @@
-chrome.extension.onMessage.addListener(function(request) {
+chrome.extension.onMessage.addListener(function (request) {
 
-	var subtitles = JSON.parse(request.subtitle)
+  var subs = JSON.parse(request.subtitle);
+  var video = document.getElementsByTagName('video')[0];
+  var subsContainer = document.createElement('div');
+  var videoPlayer = document.querySelector('#player-api');
 
-	var video = document.getElementsByTagName("video")[0]
+  videoPlayer.appendChild(subsContainer);
 
-	var subtitlesContainer = $('<div>')
-	subtitlesContainer.css({
-		position: 'absolute',
-		backgroundColor: 'black',
-		left: 0,
-		right: 0,
-		bottom: 50,
-		zIndex: 35,
-		textAlign: 'center',
-		maxWidth: '60%',
-		margin: '0 auto',
-		fontSize: 21,
-		color: 'white'
-	})
+  Object.assign(subsContainer.style, {
+    position: 'absolute',
+    backgroundColor: 'black',
+    left: 0,
+    right: 0,
+    bottom: '50px',
+    zIndex: 35,
+    textAlign: 'center',
+    maxWidth: '60%',
+    margin: '0 auto',
+    fontSize: '21px',
+    color: 'white'
+  });
 
-	$('.html5-video-content').append(subtitlesContainer)
+  function render(text) {
+    if (text) {
+      text = text[0] + '<br>' + (text[1] || '');
+      subsContainer.style.display = 'block';
+      subsContainer.innerHTML = text;
+    } else {
+      subsContainer.style.display = 'none';
+    }
+  }
 
-	function render(text) {
-		if (text) {
-			var text = text[0] + '<br>' + (text[1] || '')
-
-			if (!subtitlesContainer.is(':visible')) subtitlesContainer.show()
-
-				if (subtitlesContainer.html() !== text) {
-					subtitlesContainer.html(text)
-				}
-			} else {
-
-				if (subtitlesContainer.is(':visible')) subtitlesContainer.hide()
-
-			}
-	}
-
-	video.addEventListener("timeupdate", function() {
-
-		var currentSubtitle = subtitles.filter(function(sub) {
-			return sub.start_time <= video.currentTime && sub.end_time >= video.currentTime
-		})[0]
-
-		if (currentSubtitle) {
-			render(currentSubtitle.text)
-		} else {
-			render(null)
-		}
-	})
-})
+  video.addEventListener('timeupdate', function () {
+    var currentSub = subs.filter(function (sub) {
+      return sub.start_time <= video.currentTime && sub.end_time >= video.currentTime;
+    })[0];
+    render(currentSub ? currentSub.text : null);
+  });
+});
--- popup.js
+++ popup.js
@@ -1,39 +1,35 @@
-var baseUrl = 'http://localhost:3000/search';
+var HOST = 'http://localhost:3000';
 
-$('#search-form').on('submit', function(event) {
-	event.preventDefault()
-	var id = $(event.target).data('id')
-	var searchTerm = $('#search-box').val();
-	var request = $.get(baseUrl + '.json' + '?search=' + searchTerm);
+$('#search-form').on('submit', function (event) {
+  event.preventDefault();
+  var searchTerm = $('#search-box').val();
+  var request = $.get(`${HOST}/search.json?search=${searchTerm}`);
 
-	request.done(function(data) {
-		var list = $('<ul>')
+  request.done(function (data) {
+    var list = $('<ul>');
 
-		data.forEach(function(subtitle) {
-			var element = $('<li>')
-			element.text(subtitle.title)
-			element.attr('data-id', subtitle.id)
+    data.forEach(function (subtitle) {
+      var element = $('<li>');
+      element.text(subtitle.title);
+      element.attr('data-id', subtitle.id);
 
-			list.append(element)
-		})
+      list.append(element);
+    });
 
-		$('#subtitle-container').html(list)
-		$('#subtitle-container').on('click', 'li', function(event) {
-			event.preventDefault()
+    $('#subtitle-container').html(list);
+    $('#subtitle-container').on('click', 'li', function (event) {
+      event.preventDefault();
 
-			var subtitleId = $(event.target).attr('data-id')
-			var subtitleRequest = $.get(`http://localhost:3000/subtitles/${subtitleId}.json`)
+      var subtitleId = $(event.target).attr('data-id');
+      var subtitleRequest = $.get(`${HOST}/subtitles/${subtitleId}.json`);
 
-			subtitleRequest.done(function(data) {
-				chrome.tabs.getSelected(null, function(tab) {
-					chrome.tabs.sendMessage(tab.id, { subtitle: JSON.stringify(data) })
-				})
-			})
-		})	
-	})
-})
-
-
-
-
-
+      subtitleRequest.done(function (data) {
+        chrome.tabs.getSelected(null, function (tab) {
+          chrome.tabs.sendMessage(tab.id, {
+            subtitle: JSON.stringify(data)
+          });
+        });
+      });
+    });
+  });
+});
